3925e8849d9b693ec8a36fd1f7650ac5957c2552
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-07-25 00:41:15 +0200

    lesson update

---
+++ basic_programming/lesson_06/code/homework/Conditions.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Conditions {
+    public static void main(String[] args) {
+//        Есть две переменные  целого типа. Написать программу, выводящую на экран true, если значение одной из
+//        переменных = 10 или сумма значений переменных равна 10. Иначе на экран выводится false.
+//Пример:  int a = 10,  int b = 9   -> true
+//        int a = 1 ,  int b = 9   -> true
+//        int a = 4 ,  int b = 10  -> true
+//        int a = 4 ,  int b = 3   -> false
+
+
+    }
+}
---
+++ basic_programming/lesson_06/code/homework/HalfOfString.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class HalfOfString {
+    public static void main(String[] args) {
+//        Есть строка четной длины. Написать программу, выводящую на экран первую половину этой строки.
+//        Пример: есть Hell , результат He
+
+
+
+    }
+}
