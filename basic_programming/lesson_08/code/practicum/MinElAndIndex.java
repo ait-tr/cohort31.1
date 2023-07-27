16dab609edcd59242e6fd4666693af37e3897d4b
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-07-27 11:20:39 +0200

    lesson update

---
+++ basic_programming/lesson_08/code/practicum/MinElAndIndex.java
@@ -0,0 +1,10 @@
+package practicum;
+
+public class MinElAndIndex {
+    public static void main(String[] args) {
+//        Задан массив целых чисел: 75, 34, -15, -123, 57, -145, 86, 77, 48, -59.
+//        Найдите минимальный элемент массива и его индекс.
+
+
+    }
+}
