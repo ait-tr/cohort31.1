542e0350bc4807c87f0693d342a93c540c7b82a9
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-07-27 11:23:58 +0200

    lesson update

---
+++ basic_programming/lesson_08/code/practicum/Marks.java
@@ -0,0 +1,12 @@
+package practicum;
+
+public class Marks {
+    public static void main(String[] args) {
+
+//        Имеются оценки абитуриента из его аттестата, всего 12 оценок.
+//        Найдите средний балл абитуриента.
+
+
+
+    }
+}
