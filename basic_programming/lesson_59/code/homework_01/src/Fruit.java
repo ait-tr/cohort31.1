6c8c812b8c894abcfb8f64e7a885d7c3fda22208
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-10-09 13:16:07 +0200

    add lesson

---
+++ basic_programming/lesson_59/code/homework_01/src/Fruit.java
@@ -0,0 +1,17 @@
+public class Fruit {
+  String title;
+  double weight;
+
+  public Fruit(String title, double weight) {
+    this.title = title;
+    this.weight = weight;
+  }
+
+  @Override
+  public String toString() {
+    return "Fruit{" +
+        "title='" + title + '\'' +
+        ", weight=" + weight +
+        '}';
+  }
+}
