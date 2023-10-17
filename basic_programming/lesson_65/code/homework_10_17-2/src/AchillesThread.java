2fbd256d3352aa909007ecafb540c42b1bd2e04b
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-10-17 13:47:36 +0200

    add lesson

---
+++ basic_programming/lesson_65/code/homework_10_17-2/src/AchillesThread.java
@@ -0,0 +1,13 @@
+public class AchillesThread extends Thread{
+  @Override
+  public void run(){
+    for(int i = 0; i< 30; i++){
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      System.out.println("I run fast");
+    }
+  }
+}
