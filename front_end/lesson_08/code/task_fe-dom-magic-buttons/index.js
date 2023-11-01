ce7118ed71afe2ed4acc27fb92b9ad6c085ccfbd
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-11-01 13:49:50 +0100

    add lesson

---
+++ front_end/lesson_08/code/task_fe-dom-magic-buttons/index.js
@@ -0,0 +1,23 @@
+// найдем первую кнопку по id
+const btn = document.getElementById("magic-btn");
+// скопируем ее - получилась вторая кнопка. Ее можно использовать.
+const clonedBtn = btn.cloneNode(true);
+// задали id
+clonedBtn.id = "magic-btn-2";
+clonedBtn.textContent = "Я изменю тебя";
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
+clonedBtn.classList.add("second-btn");
+
+btn.addEventListener('click', () => {
+  // при нажатии на первую кнопку
+  // работайте с clonedBtn
+  // ваш код начинается здесь
+  document.body.append(clonedBtn);
+});
+
+// здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', ()=>{
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
+});
