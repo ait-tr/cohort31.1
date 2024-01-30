eff09d0df84bb448dbe718564714192d290d136f
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-01-30 14:07:31 +0100

    lesson update

--- back_end/lesson_13/code/api-REST-demo-OLD/src/main/java/de/ait/apirestdemo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package de.ait.apirestdemo.repository;
-
-import de.ait.apirestdemo.entity.User;
-
-public interface UserRepository extends CrudRepository<User>{
-    User findOneByEmail(String email);
-}
---
+++ back_end/lesson_14/code/README.md
@@ -0,0 +1,4 @@
+Проект event-mvc-demo:
+
+Проект api-REST-demo:
+https://github.com/ait-tr/cohort31.1/tree/main/back_end/lesson_14/code/api-REST-demo/src
