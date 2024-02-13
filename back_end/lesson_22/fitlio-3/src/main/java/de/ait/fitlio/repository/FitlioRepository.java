4b50e4b0127d60d433a5af1cf9469087bc6fd5a5
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-02-13 14:29:37 +0100

    Lesson update

--- back_end/lesson_22/fitlio-3/src/main/java/de/ait/fitlio/repository/FitlioRepository.java
+++
@@ -1,8 +0,0 @@
-package de.ait.fitlio.repository;
-
-import de.ait.fitlio.model.Fitlio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FitlioRepository extends JpaRepository<Fitlio, Long> {
-
-}
--- back_end/lesson_22/fitlio-3/src/main/java/de/ait/fitlio/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package de.ait.fitlio.repository;
-
-import de.ait.fitlio.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
